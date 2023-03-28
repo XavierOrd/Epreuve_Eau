3c641029c8522dabd8a9e2d19dadcb28c493dd5d
XO <xavierordonneau@gmail.com>
2023-03-28 16:53:01 +0200

    Ajout exercice 01

--- Eau01.js
+++ Eau01.js
@@ -3,125 +3,127 @@
 let concatNumber = (letter, cumul) => cumul + letter;
 
 let arrayToString = (myArray) => {
+    //on transforme le tableau en chaine ['1','0','2'] devient '102'
     let numberComposed = '';
     for (var i = 0, len = myArray.length; i < len; i++) {
         numberComposed = concatNumber(myArray[i], numberComposed);
     }
-    return numberComposed
+    return numberComposed;
 };
 
-let combinaisonExist = (combinaison, tabListeNB) => {
-    //pour chaque caractère du chiffre, on contrôle le nombre de fois où il est présent 
-    if (tabListeNB.indexOf(combinaison) === -1) {
+let combinaisonExist = (inputCombinaison, inputArrNumbers) => {
+    //on contrôle si la combinaison existe déjà dans le tableau des chiffres valide
+    if (inputArrNumbers.indexOf(inputCombinaison) === -1) {
         return false;
     } else {
         return true;
     }
 };
 
-function nbOccurence(chaineComplete) {
-    let numberToArray = chaineComplete.split('');
+function nbOccurence(inputStringNumber) {
+    //Pour un chiffre, on contrôle si il y a des nombres identique
+    let stringToArray = inputStringNumber.split('');
     let nbOccurence = 0;
-    for (let i = 0; i < numberToArray.length; i++) {
-        if (numberToArray.indexOf(numberToArray[i]) !== numberToArray.lastIndexOf(numberToArray[i])) {
+
+    for (let i = 0; i < stringToArray.length; i++) {
+        if (stringToArray.indexOf(stringToArray[i]) !== stringToArray.lastIndexOf(stringToArray[i])) {
             nbOccurence++;
             break;
         }
     }
     if (nbOccurence > 0) {
-        return false
+        return false;
     } else {
-        return true
+        return true;
     }
 };
 
-function addValidNumbers0To999() {
+function addValidString000To999() {
+    //on ajoute dans un tableau, les chiffre de 1 à 999 au format '001'
+    //on exclut les chiffres ayant plus de 2 nombres identiques, exemple : 002
     let arrayNumbers = new Array;
+    let isValidNumber = true;
     for (let index = 0; index < 999; index++) {
-        let nbFormate = '';
-        nbFormate = index.toString();
+        let numberToString = '';
+        numberToString = index.toString(); 
+        numberToString = numberToString.padStart(3, '0'); //1 devient 001
 
-        nbFormate = nbFormate.padStart(3, '0');
+        // if (numberToString == '012') {
+        //     let test1 = ''
+        // }
+        isValidNumber = nbOccurence(numberToString)
 
-        if (nbFormate == '012') {
-            let test1 = ''
-        }
-        if (nbFormate == '987') {
-            let test = ''
-        }
-
-        let concerveNumber = nbOccurence(nbFormate)
-
-        if (!concerveNumber) {
+        if (!isValidNumber) {
             continue;
         }
-        arrayNumbers.push(nbFormate);
+        arrayNumbers.push(numberToString);
     }
     return arrayNumbers;
 };
 
-let isExistsCombinaison = (inputNumber, inputArray) => {
-    //on parcours chaque nombre de la chaine pour le déplacer et calculer des combinaisons possible 
-    //Exemple, le chiffre 789 donne :
+let combinaisonAlreadyExists = (inputNumber, inputArray) => {
+    //on parcours chaque nombre du chiffre pour le déplacer et calculer des combinaisons possible 
+    //Exemple, le chiffre 789, en déplacant le nombre 7, donne :
     //  789
     //  879
     //  897
     // ...
     let myNumberFormated = inputNumber.split(''); //on convertie la chaine en tableau 
-    let nb = 0
     let shouldPass = false;
     let combinaison = '';
     let combinaisonReverse = '';
 
+    //on va déplacer chaque nombre du chiffre
+    // Exemple pour 789, on va déplacer le nombre 7, puis le nombre 8, puis le nombre 9 pour créer des combinaisons
     for (let numberToMove = 0; numberToMove < myNumberFormated.length; numberToMove++) {
         if (shouldPass) {
             //on a déjà trouvé une combinaison qui existe donc inutile de continuer à traiter ce chiffre 
-            //Exemple : 021 n’est pas là parce que 012 est présent
+            //Exemple : 021 donne la combinaison 012 et 012 est déjà présent dans la liste des nombres valide
             break;
         }
         //seconde boucle pour obtenir la position sur laquelle déplacer le numberToMove
+        // Exemple, on déplace le nombre 7, en position 0 puis 1 puis 2
         for (let pos = 0; pos < myNumberFormated.length; pos++) {
 
-            let nouveauTableau = myNumberFormated.slice(); //on copie le tableau pour le manipuler librement en conservant l'ancien
-            let numberDeleted = nouveauTableau.splice(numberToMove, 1); //on supprime le nombre à bouger du tableau pour le réinserer dans une nouvelle position 
-            nouveauTableau.splice(pos, 0, numberDeleted.toString()); //on insère le nombre supprimé dans sa nouvelle position pour créer une combinaison Exemple 789->879
+            let arrayCombinaison = myNumberFormated.slice(); //on copie le tableau pour le manipuler librement en conservant l'ancien d'origine
+            let numberDeleted = arrayCombinaison.splice(numberToMove, 1); //on supprime le nombre à bouger du tableau pour le réinserer dans une nouvelle position (attention numberDeleted est un tableau)
+            arrayCombinaison.splice(pos, 0, numberDeleted.toString()); //on insère le nombre supprimé dans sa nouvelle position pour créer une combinaison Exemple 789->879
 
             combinaison = '';
             combinaisonReverse = '';
-            combinaison = arrayToString(nouveauTableau); // on transforme le combinaison (Array) crée en String    
+            combinaison = arrayToString(arrayCombinaison); // on transforme le combinaison (Array) crée en String    
 
-            shouldPass = combinaisonExist(combinaison, inputArray) // on contrôle si la combinaison crée, existe dans la liste totale des chiffres
+            shouldPass = combinaisonExist(combinaison, inputArray) // on contrôle si la combinaison crée, existe dans la liste totale des chiffres valide
             if (!shouldPass) {
-                //si la combinaison ne passe pas, on teste la même combinaison en reverse : cas du  987, il faut tester aussi 789 
-                nouveauTableau.reverse()
-                combinaisonReverse = arrayToString(nouveauTableau)
+                //si la combinaison n'existe pas, on teste la même combinaison en reverse : cas du  987, il faut tester aussi 789 qui est déjà présent
+                arrayCombinaison.reverse()
+                combinaisonReverse = arrayToString(arrayCombinaison)
                 shouldPass = combinaisonExist(combinaisonReverse, inputArray)
             }
             if (shouldPass) {
+                //on a trouvé une combinaison qui existe donc inutile de continuer à traiter ce chiffre 
                 break;
             }
-
         }
     }
     return shouldPass
 };
 
 let addValidNumbers = () => {
-    let tabAllNumbers = new Array;
-    let tabListeNB = new Array;
+    let arrAllNumbers = new Array;
+    let arrFinalResult = new Array;
 
-    tabAllNumbers = addValidNumbers0To999();
+    arrAllNumbers = addValidString000To999();
 
-    for (let index = 0; index < tabAllNumbers.length; index++) {
+    for (let index = 0; index < arrAllNumbers.length; index++) {
 
-        if (!isExistsCombinaison(tabAllNumbers[index],tabListeNB) ) {
+        if (!combinaisonAlreadyExists(arrAllNumbers[index],arrFinalResult) ) {
             //si le chiffre et ses combinaisons possible n'existe pas dans le tableau, on l'ajoute
-            tabListeNB.push(tabAllNumbers[index]);
+            arrFinalResult.push(arrAllNumbers[index]);
         }
-
     }
-    tabListeNB.sort(); //on trie le tableau
-    return tabListeNB;
+    arrFinalResult.sort(); //on trie le tableau
+    return arrFinalResult;
 };
 
 let arrayValidNumbers = addValidNumbers();
